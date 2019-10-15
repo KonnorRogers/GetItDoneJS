b23855b45badcde690a71a8b063668a88ab8ad18
paramagicdev <konnor5456@gmail.com>
2019-10-15 09:24:56 -0400

    add babel and sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,36 @@ module.exports = {
     filename: 'bundle.js',
   },
 
+  // CSS rules
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+
+      // ES5 transpiling
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+
+  // webpack-dev-server config
   devServer: {
     open: true,
     contentBase: path.resolve(__dirname, 'dist'),
