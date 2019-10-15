79101606190ba1c63b7307436f91c8305a98f580
paramagicdev <konnor5456@gmail.com>
2019-10-15 11:38:51 -0400

    added dommanipulator testing

--- __tests__/domManipulator.test.js
+++ __tests__/domManipulator.test.js
@@ -1,9 +1,36 @@
-import DomManipulator from './src/js/domManipulator.js';
+import DomManipulator from '../src/js/domManipulator.js';
 
-test('Should render an element with an ID of test', () => {
-  const props = {};
-  props.id = 'test';
-  const element = DomManipulator.renderElement(props);
+describe('DomManipulator()', () => {
+  describe('renderElement()', () => {
+    test('Should render an element with an ID of test', () => {
+      const props = {};
+      props.type = 'div';
+      props.id = 'test';
+      const element = DomManipulator.renderElement(props);
 
-  expect(element.id).toBe('test');
+      expect(element).not.toBeNull();
+      expect(element).toBeInstanceOf(HTMLElement);
+      expect(element.id).toBe('test');
+    });
+
+    test('Should apply nonvalid element fields', () => {
+      const props = {};
+      props.type = 'p';
+      props.invalidField = 'invalid';
+      const element = DomManipulator.renderElement(props);
+      expect(element).not.toBeNull();
+      expect(element.invalidField).toBe('invalid');
+    });
+
+    test('Should return null if given no values', () => {
+      const element = DomManipulator.renderElement();
+      expect(element).toBeNull();
+    });
+
+    test('Should apply valid html values if assigned after create', () => {
+      const element = DomManipulator.renderElement({type: 'p'});
+      element.id = 'new ID';
+      expect(element.id).toBe('new ID');
+    });
+  });
 });
--- src/js/domManipulator.js
+++ src/js/domManipulator.js
@@ -1,13 +1,19 @@
 export default (function DomManipulator() {
   const renderElement = (props = {}) => {
     const object = Object.assign({}, props);
+
+    // If no type is defined, return null
+    if (!props.type) {
+      return null;
+    }
+
     const element = document.createElement(`${object.type}`);
 
     // Assigns properties to the element
     // IE: props.onclick = () => function
     // Will then be applied to the element
-    for (const [key, element] of Object.entries(object)) {
-      element[`${key}`] = element;
+    for (const [key, value] of Object.entries(object)) {
+      element[`${key}`] = value;
     }
 
     return element;
