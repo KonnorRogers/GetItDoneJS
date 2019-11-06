0b287e5cc320183224aecac42fe3553e15267d97
paramagicdev <konnor5456@gmail.com>
2019-11-06 10:44:01 -0500

    adding additonal components w/o implementations

--- src/js/components/addItemForm.comp.js
+++
@@ -1,7 +0,0 @@
-import DomManipulator from '../utils/domManipulator.js';
-import {Base} from './base.comp.js';
-
-export const addItemForm = (props = {}) => {
-  const _base = Base(props);
-  const _baseProps = _base.getProps();
-};
--- src/js/components/addProjectForm.comp.js
+++ src/js/components/addProjectForm.comp.js
@@ -1,1 +1,18 @@
-export const projectForm = () => {};
+import DomManipulator from '../utils/domManipulator.js';
+import {Base} from './base.comp.js';
+
+export const projectForm = (props = {}) => {
+  const _base = Base(props);
+  const _baseProps = _base.getProps();
+
+  const onclick = () => {};
+
+  const render = () => {
+    const form = DomManipulator.createElement(_baseProps.element, _baseProps);
+    return form;
+  };
+
+  return {
+    render,
+  };
+};
