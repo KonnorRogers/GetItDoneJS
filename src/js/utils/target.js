c9fd039c8a9e4ddc5c9618fbab4bd035fd643793
paramagicdev <konnor5456@gmail.com>
2019-11-14 10:48:24 -0500

    Everything works as expected now

--- src/js/utils/target.js
+++ src/js/utils/target.js
@@ -1,14 +1,15 @@
-import DomManipulator from '';
+import DomManipulator from './domManipulator.js';
+
 export const Target = () => {
   const handleTargets = () => {
     const tabs = ['#tasks', '#projects'];
-    _base.dom().setTarget(tabs, tabs[0]);
+    DomManipulator.setTarget(tabs, tabs[0]);
 
     window.addEventListener('hashchange', e => {
       // Prevent scrolling
       e.target.scrollTo(0, 0);
 
-      _base.dom().setTarget(tabs, tabs[1]);
+      DomManipulator.setTarget(tabs, tabs[1]);
     });
   };
 
