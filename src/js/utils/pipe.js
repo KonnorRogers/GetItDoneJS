41a441650379a292ca9cbaad2da8b1b2e8285690
paramagicdev <konnor5456@gmail.com>
2019-11-20 11:06:21 -0500

    change description of pipe

--- src/js/utils/pipe.js
+++ src/js/utils/pipe.js
@@ -2,10 +2,8 @@
  * pipe() is a utility function which will add multiple functions onto it. The functions passed in
  * should take in an {Object} parameter.
  * This function is similar to {@link https://lodash.com/docs/#flow|lodash's flow function}. It is taken from Eric Elliot.
- *
- * @param {...Function|Function[]} ...fns - Given an array of functions, pipe them into an object
- * @return {Function({Object})} Returns a function with multiple functions added to it. The function takes in an Object
- *   to pipe functions onto.
+ * @see {@link ItemMixin} for further usage
+ * @see {@link https://medium.com/javascript-scene/javascript-factory-functions-with-es6-4d224591a8b1#ca97|pipe function}
  *
  * @example
  * const createDrone = ({capacity = '3000mAh'}) => pipe(
@@ -14,7 +12,8 @@
  *     withConstructor(createDrone)
  *   )({});
  *
- * @see {@link https://medium.com/javascript-scene/javascript-factory-functions-with-es6-4d224591a8b1#ca97|pipe function}
- * @see {@link ItemMixin} for further usage
+ * @param {...Function|Function[]} ...fns - Given an array of functions, pipe them into an object
+ * @return {Function({Object})} Returns a function with multiple functions added to it. The function takes in an Object
+ *   to pipe functions onto.
  */
 export const pipe = (...fns) => x => fns.reduce((y, f) => f(y), x);
